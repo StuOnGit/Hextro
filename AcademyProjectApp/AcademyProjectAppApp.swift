a2ad39a1b215cdc19b1c26b1049c6c806943ec9a
valikleha <57154611+valikleha@users.noreply.github.com>
2022-10-20 13:43:22 +0200

    Initial Commit

---
+++ AcademyProjectApp/AcademyProjectAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AcademyProjectAppApp.swift
+//  AcademyProjectApp
+//
+//  Created by Alexey on 20/10/22.
+//
+
+import SwiftUI
+
+@main
+struct AcademyProjectAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
