f717175b52a62f14fe2245f89f4f1712f1363e6b
Stusio <france.destasio@studenti.unina.it>
2022-10-26 15:33:39 +0200

    Add Goals View

---
+++ AcademyProjectApp/View/GoalsView.swift
@@ -0,0 +1,20 @@
+//
+//  GoalsView.swift
+//  AcademyProjectApp
+//
+//  Created by Francesco De Stasio on 26/10/22.
+//
+
+import SwiftUI
+
+struct GoalsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GoalsView_Previews: PreviewProvider {
+    static var previews: some View {
+        GoalsView()
+    }
+}
