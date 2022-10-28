4b392f54997bd097853e6a18895bbde2b08eeea5
valikleha <57154611+valikleha@users.noreply.github.com>
2022-10-28 14:26:38 +0200

    Onboarding screen
    Create onboarding screen

---
+++ AcademyProjectApp/ContentView 2.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  AcademyProjectApp
+//
+//  Created by Alexey on 20/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
