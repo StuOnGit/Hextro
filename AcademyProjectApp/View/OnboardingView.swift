299b069e15ca7845ccb18d4c873387eff452a4d5
valikleha <57154611+valikleha@users.noreply.github.com>
2022-10-31 22:04:27 +0100

    Login screen and profile section (again)

--- AcademyProjectApp/View/OnboardingView.swift
+++ AcademyProjectApp/View/OnboardingView.swift
@@ -16,85 +16,105 @@ struct OnboardingView: View {
     @State private var name: String = ""
     @State private var surname: String = ""
     @State var selectedItems:[PhotosPickerItem] = []
-    @State var data: Data?
-
+    @State var avatar: Data?
+    @AppStorage("userAvatar") var userAvatar: Data?
+    
     var body: some View {
-      
+        
         VStack (alignment: .center) {
             
-                Text ("Welcome!")
-               
-                    .font(.system(size: 50))
-                    .fontWeight(.bold)
-                    .multilineTextAlignment(.center)
-                    .padding(.top, 60)
+            Text ("Welcome!")
+            
+                .font(.system(size: 50))
+                .fontWeight(.bold)
+                .multilineTextAlignment(.center)
+                .padding(.top, 70)
             Spacer()
-            if let data = data, let uiimage = UIImage(data: data) {
+            
+            if let data = avatar, let uiimage = UIImage(data: data) {
                 Image(uiImage: uiimage)
                     .resizable()
+                    .scaledToFill()
+                    .frame(width: 150, height: 150)
+                    .clipShape(Circle())
+                    .padding(.bottom, 20)
             }
             PhotosPicker(
                 selection: $selectedItems, maxSelectionCount: 1, matching: .images) {
-                    Image(systemName: "photo")
-                        .font(.title.bold())
-                        .frame(width: 150, height: 150)
-                        .foregroundColor(Color.white)
-                        .background(Color.blue)
-                        .clipShape(Circle())
+                    Text ("Choose Profile Photo")
                     
+                }
+            
+                .onChange(of: selectedItems) { newValue in
+                    guard let item = selectedItems.first else {
+                        return
+                    }
+                    item.loadTransferable(type: Data.self) { result in
+                        switch result {
+                        case .success(let data):
+                            if let data = data {
+                                self.avatar = data
+                            } else {
+                                print("Data is nil")
+                            }
+                        case .failure(let failure):
+                            fatalError("Error \(failure)")
+                        }
+                    }
                 }
             HStack {
                 Text("Name")
-                    .padding(.top, 50)
+                    .padding(.top, 30)
                     .padding(.leading, 20)
                     .font(.system(.title2).bold())
                 Spacer()
-                    
-
-            }
                 
-                TextField("Enter your name", text: $name)
+                
+            }
+            
+            TextField("Enter your name", text: $name)
                 .textFieldStyle(.roundedBorder)
                 .padding(.trailing, 20)
                 .padding(.leading, 20)
-           
-                  
+            
+            
             HStack {
                 Text("Surname")
                     .padding(.leading, 20)
                     .font(.system(.title2).bold())
                 Spacer()
-                    
-
+                
+                
             }
             TextField("Enter your surname", text: $surname)
                 .textFieldStyle(.roundedBorder)
                 .padding(.trailing, 20)
                 .padding(.leading, 20)
-                Button(action: {
-                    userOnboarded = true
-                    userName = name
-                    userSurname = surname
-                })
-                {
-                    
-                    Text ("Continue")
-                        .foregroundColor(.white)
-                        .font(.body.bold())
-
-                        
-                        .background {
-                            RoundedRectangle(cornerRadius: 10)
-                                .frame(width: 350, height: 50)
-                                .foregroundColor(.blue)
-                        }
-                        .padding(.top, 80)
-                        .padding(.bottom, 70)
-                }
-            
+            Button(action: {
+                userOnboarded = true
+                userName = name
+                userSurname = surname
+                userAvatar = avatar
+            })
+            {
                 
-            
+                Text ("Continue")
+                    .foregroundColor(.white)
+                    .font(.body.bold())
                 
+                
+                    .background {
+                        RoundedRectangle(cornerRadius: 10)
+                            .frame(width: 350, height: 50)
+                            .foregroundColor(.blue)
+                    }
+                    .padding(.top, 80)
+                    .padding(.bottom, 70)
+            }
+            
+            
+            
+            
             
         }
     }
